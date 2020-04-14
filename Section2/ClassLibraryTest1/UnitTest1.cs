3cdc6150eb87f00c78280f43d835a0789cce0119
CiRcA1991 <CiRcA1991@users.noreply.github.com>
2020-04-14 19:05:41 +0100

    Version incompability fix 1.0
    Fixing incompatibility version of VS 2019 with VS 2017/2015

--- Section2/ClassLibraryTest1/UnitTest1.cs
+++ Section2/ClassLibraryTest1/UnitTest1.cs
@@ -1,17 +1,17 @@
-﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace ClassLibraryTest
+namespace ClassLibraryTest1
 {
     [TestClass]
     public class UnitTest1
     {
+        private readonly object ClassLibraryPractice;
+
         [TestMethod]
         public void TestMethod1()
         {
             int result = ClassLibraryPractice.Class1.Addition(10, 5);
             Assert.AreEqual(result, 15);
-
         }
     }
 }
