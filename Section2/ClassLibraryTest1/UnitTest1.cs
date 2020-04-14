2984d3ee33e26de81ea174ac09007a8ce35ffcb5
CiRcA1991 <CiRcA1991@users.noreply.github.com>
2020-04-14 20:13:25 +0100

    Incompatibilitie fix 1.1
    Com a ajuda no nuno

--- Section2/ClassLibraryTest1/UnitTest1.cs
+++ Section2/ClassLibraryTest1/UnitTest1.cs
@@ -1,17 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+namespace ClassLibraryPractice
+{
+    public static class Class1
+    {
+        public static int Addition(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
+
+
 namespace ClassLibraryTest1
 {
     [TestClass]
     public class UnitTest1
     {
-        private readonly object ClassLibraryPractice;
 
+        
         [TestMethod]
         public void TestMethod1()
         {
             int result = ClassLibraryPractice.Class1.Addition(10, 5);
-            Assert.AreEqual(result, 15);
+            Assert.AreEqual(result, 14);
         }
     }
 }
